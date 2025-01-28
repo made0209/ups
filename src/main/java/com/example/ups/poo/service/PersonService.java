4b47af8af64784e061f4f13f1989f2982a393b71
made0209 <mortizm8@est.ups.edu.ec>
2025-01-28 09:36:05 -0500

    Add(I): Add Person Method By Id

--- src/main/java/com/example/ups/poo/service/PersonService.java
+++ src/main/java/com/example/ups/poo/service/PersonService.java
@@ -1,6 +1,9 @@
 package com.example.ups.poo.service;
 
 import com.example.ups.poo.dto.Person;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -8,12 +11,28 @@ import java.util.List;
 
 @Service
 public class PersonService {
+
+    List<Person> personList = new ArrayList<>();
+
     public List<Person> getAllPeople() {
-        List<Person> personList = new ArrayList<>();
-        Person p1 = new Person("Madeleine", "Ortiz", 19, "0953340312");
+        Person p1 = new Person();
+        p1.setName("Madeleine");
+        p1.setLastname("Ortiz");
+        p1.setAge(19);
+        p1.setId("0953340312");
+
         Person p2 = new Person("Débora", "Benavides", 20, "0918818766");
         personList.add(p1);
         personList.add(p2);
         return personList;
+    }
+
+    public ResponseEntity getPersonById(String id) {
+        for (Person person : personList) {
+            if (id.equalsIgnoreCase(person.getId())) {
+                return ResponseEntity.status(HttpStatus.OK).body(person);
+            }
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Person with id: " + id + " not found");
     }
 }
